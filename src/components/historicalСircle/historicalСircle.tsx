f61c6e75845dadac8006874a8c44da981c3c805d
MatykinaAnna <matykina2000@gmail.com>
2025-06-17 21:53:33 +0300

    корректная анимация окружности

--- src/components/historicalСircle/historicalСircle.tsx
+++ src/components/historicalСircle/historicalСircle.tsx
@@ -1,6 +1,10 @@
 import style from './style.module.scss'
 import * as d3 from 'd3';
 import { historyEvent } from '../../shared/interface';
+import classNames from 'classnames';
+
+import btn from '../../shared/icons/btn.png'
+
 import {
     useEffect,
     useRef,
@@ -19,26 +23,32 @@ const HistoricalСircle = (props: {
         }) => {
 
     const svgRef = useRef(null);
+    const svgWrapperCircle = useRef(null);
 
     const [dataHistoryEvents, setDataHistoryEvents] = useState(props.historyEvents)
     const [activeHisEv, setActiveHisEv] = useState(0)
 
     useEffect(() => {
+
+        console.log(dataHistoryEvents)
+
         const svg = d3.select(svgRef.current);
         const width = +svg.attr('width');
         const height = +svg.attr('height');
         
-        svg.attr("height", height)
+        if (document.getElementsByClassName('isCircle').length == 0){
+            svg.attr("height", height)
             .attr("width", width);       
                 
-        svg.append("circle") 
-                .attr("cx", props.cx)
-                .attr("cy", props.cy)
-                .attr("r", props.r)
-                .style("stroke", '#d5d7da')
-                .style("fill", "none");
-
-    }, []);
+            svg.append("circle") 
+                    .attr("cx", props.cx)
+                    .attr("cy", props.cy)
+                    .attr("r", props.r)
+                    .style("stroke", '#d5d7da')
+                    .style("fill", "none");
+        }
+        
+    }, [dataHistoryEvents]);
 
     useEffect(()=>{
 
@@ -46,38 +56,150 @@ const HistoricalСircle = (props: {
         let corner = 270 / props.historyEvents.length        
         let corner1 = 0
 
-        for (let i=0; i< dataHistoryEvents.length; i++){
-            
-            
-            let x = props.cx + props.r * Math.cos(corner1)
-            let y = props.cy + props.r * Math.sin(corner1)
+        let x = props.cx + props.r * Math.cos(corner1)
+        let y = props.cy + props.r * Math.sin(corner1)
 
-            let color = 'gray'
+        let color = 'gray'
 
+        if (document.getElementsByClassName('isCircle').length == 0){
             svg.append("circle") 
                 .attr("cx", x)
                 .attr("cy", y)
-                .attr("r", 2)
+                .attr("r", 20)
                 .style("stroke", 'gray')
-                .style("fill", color);
+                .style("fill", 'white')
+                .classed('isCircle', true)
+                        
+            svg.append("text")
+                .text(`${activeHisEv+1}`)
+                .attr("x", x-4)
+                .attr("y", y+5)
+                .classed('isText', true)
 
             corner1 = corner1 + corner    
-        }        
+
+            console.log('activeHisEv', activeHisEv)
+
+            for (let i=activeHisEv+1; i< dataHistoryEvents.length; i++){
+                console.log(i)
+                x = props.cx + props.r * Math.cos(corner1)
+                y = props.cy + props.r * Math.sin(corner1)
+                svg.append("circle") 
+                        .attr("cx", x)
+                        .attr("cy", y)
+                        .attr("r", 2)
+                        .style("stroke", 'gray')
+                        .style("fill", color);
+                corner1 = corner1 + corner    
+            } 
+            for (let i=0; i< activeHisEv; i++){
+                console.log(i)
+                x = props.cx + props.r * Math.cos(corner1)
+                y = props.cy + props.r * Math.sin(corner1)
+
+                svg.append("circle") 
+                        .attr("cx", x)
+                        .attr("cy", y)
+                        .attr("r", 2)
+                        .style("stroke", 'gray')
+                        .style("fill", color);
+
+                corner1 = corner1 + corner    
+            }
+
+        }
+        
 
     }, [dataHistoryEvents])
+
+    function toBack(){
+        console.log(activeHisEv)
+        if (activeHisEv > 0){
+            setActiveHisEv(activeHisEv-1)
+            const svg = svgRef.current;
+            const corner = 360 / props.historyEvents.length
+            //@ts-ignore
+            svg.style.transform = `rotate(${corner}deg)`
+            
+            document.getElementsByClassName('isText')[0].remove()
+            const svgd3 = d3.select(svgRef.current);
+            svgd3.selectChild('.isCircle')
+            .transition()
+                .attr("r", 2)
+                .style("fill", 'gray') 
+            //@ts-ignore
+            svg.style.transition = 'transform 1s'
+            setTimeout(()=>{
+                //@ts-ignore
+                svg.innerHTML = ''
+                //@ts-ignore
+                svg.style.transform = `rotate(0deg)`
+                //@ts-ignore
+                svg.style.transition = 'transform 0s'
+                let array = []
+                for (let i=activeHisEv-1; i<dataHistoryEvents.length; i++){
+                    array.push(dataHistoryEvents[i])
+                }
+                for (let i=0; i<activeHisEv-1; i++){
+                    array.push(dataHistoryEvents[i])
+                }
+                setDataHistoryEvents(array)
+            }, 1000)
+        }
+    }
+
+    function toForward(){
+        if (activeHisEv < dataHistoryEvents.length-1){
+            setActiveHisEv(activeHisEv+1)
+            const svg = svgRef.current;
+            const corner = 360 / props.historyEvents.length
+            //@ts-ignore
+            svg.style.transform = `rotate(${-corner}deg)`
+            console.log(document.getElementsByClassName('isText')[0])
+            document.getElementsByClassName('isText')[0].remove()
+
+            const svgd3 = d3.select(svgRef.current);
+            svgd3.selectChild('.isCircle')
+            .transition()
+                .attr("r", 2)
+                .style("fill", 'gray')          
+
+            //@ts-ignore
+            svg.style.transition = 'transform 1s'
+            setTimeout(()=>{
+                //@ts-ignore
+                svg.innerHTML = ''
+                //@ts-ignore
+                svg.style.transform = `rotate(0deg)`
+                //@ts-ignore
+                svg.style.transition = 'transform 0s'
+                let array = []
+                for (let i=activeHisEv+1; i<dataHistoryEvents.length; i++){
+                    array.push(dataHistoryEvents[i])
+                }
+                for (let i=0; i<activeHisEv+1; i++){
+                    array.push(dataHistoryEvents[i])
+                }
+                setDataHistoryEvents(array)
+            }, 1000)
+        }
+    }
     
     return (
         <div className={style.wrapper}>
-            <div>
-                <div>{activeHisEv}/{dataHistoryEvents.length}</div>
-                <div>
-                    <button className={'btn-back'}></button>
-                    <button className={'btn-forward'}></button>
-                </div>
-            </div>
             <div className={style.wrapperCircle}>
-                <svg ref = {svgRef} width = {String(props.width)} height = {String(props.height)}> </svg>
+                <svg ref = {svgRef} className={style.wrapperCircle} width = {String(props.width)} height = {String(props.height)}> </svg>
             </div>
+            <div className={style.wrapperButtons}>
+                <div className={style.divButtons}>
+                    <div>{activeHisEv+1}/{dataHistoryEvents.length}</div>
+                    <div>
+                        <img src={btn} onClick={toBack} className={style.btnBack} alt="btnBack" />
+                        <img src={btn} onClick={toForward} className={style.btnForward} alt="btnBack" />
+                    </div>
+                </div>
+                
+            </div>            
         </div>
         
     )
